3a8e2ffa729b2ae467971d5b77730e41e9ac7371
Rohin Adalja <rohin.adalja@mail.utoronto.ca>
2020-03-07 18:34:18 -0500

    create block model

--- models/block.js
+++ models/block.js
@@ -1,13 +1,41 @@
+const crypto = require('crypto');
+
 class Block {
-  constructor(inIndex, inPrevBlockHash, inPrevProof, inTransactions) {
-    this.index = inIndex;
-    this.proof = inPrevProof;
-    this.previousBlockHash = inPrevBlockHash;
-    this.transactions = inTransactions;
+  constructor(index, transaction, prevHash, prevProof='rohin-proof') {
+    this.index = index;
     this.timestamp = Date.now();
+    this.transaction = transaction;
+    this.proof = prevProof;
+    this.prevHash = prevHash;
+    this.hash=this.getHashVal();
+}
+  
+  getHashVal() {
+    const blockStrToHash = JSON.stringify(this.transaction) + this.prevHash + this.index + this.timestamp + JSON.stringify(this.proof);
+    const hash = crypto
+      .createHmac('sha256', "rohin-secret")
+      .update(blockStrToHash)
+      .digest('hex');
+
+    return hash;
   }
 
   /* Block class related functions here */ 
+  setProof(inProof) {
+    this.proof = inProof;
+  }
+
+  getProof() {
+    return this.proof;
+  }
+
+  getIndex() {
+    return this.index;
+  }
+
+  getPreviousBlockHash() {
+    return this.previousBlockHash;
+  }
 }
 
 module.exports = Block;

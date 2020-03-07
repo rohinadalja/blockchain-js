c66293fb311a5af94b5d3ac63c132d82f4fb2c75
Rohin Adalja <rohin.adalja@mail.utoronto.ca>
2020-03-07 18:35:04 -0500

    create blockchain model using block class

---
+++ models/blockchain.js
@@ -0,0 +1,26 @@
+const Block = require('./block');
+
+class BlockChain {
+  constructor() {
+      this.chain = [];
+  }
+
+  addBlock(data) {
+      let index = this.chain.length;
+      let prevHash = this.chain.length !== 0 ? this.chain[this.chain.length - 1].hash : 0;
+      let block = new Block(index, data, prevHash, 'rohin-proof');
+      this.chain.push(block);
+  }
+
+  chainIsValid(){
+          for(var i=0;i<this.chain.length;i++){
+              if(this.chain[i].hash !== this.chain[i].getHashVal())
+                  return false;
+              if(i > 0 && this.chain[i].prevHash !== this.chain[i-1].hash)
+                  return false;
+          }
+          return true;
+      }
+}
+
+module.exports = BlockChain;
